9087fe2465aceb6f3b07ccb65f732731b23f9a94
Kenzie <kenzie_davidson@cate.org>
2020-01-13 09:55:58 -0800

    Initial Commit

---
+++ Made To Order/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Made To Order
+//
+//  Created by Grace Davidson on 1/13/20.
+//  Copyright © 2020 kenzie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Made To OrderTests/Made_To_OrderTests.swift
@@ -0,0 +1,34 @@
+//
+//  Made_To_OrderTests.swift
+//  Made To OrderTests
+//
+//  Created by Grace Davidson on 1/13/20.
+//  Copyright © 2020 kenzie. All rights reserved.
+//
+
+import XCTest
+@testable import Made_To_Order
+
+class Made_To_OrderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
